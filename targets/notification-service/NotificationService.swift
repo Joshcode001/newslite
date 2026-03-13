8fc122b6d05701885fd7a1b0aaaa7d67a6211f8f
[Joshcode001] <[aristocrat.je@gmail.com]>
2026-03-13 14:45:53 +0100

    jhgfy88

--- targets/notification-service/NotificationService.swift
+++ targets/notification-service/NotificationService.swift
@@ -13,9 +13,8 @@ class NotificationService: UNNotificationServiceExtension {
       (request.content.mutableCopy() as? UNMutableNotificationContent)
 
     if let bestAttemptContent = bestAttemptContent {
-      if let userInfo = request.content.userInfo["body"] as? [String: Any],
-         let rich = userInfo["richContent"] as? [String: Any],
-        let imageUrlString = rich["image"] as? String,
+      if let userInfo = request.content.userInfo["richContent"] as? [String: Any],
+        let imageUrlString = userInfo["image"] as? String,
         let imageUrl = URL(string: imageUrlString) {
         downloadAndAttachImage(url: imageUrl, to: bestAttemptContent) { content in
           contentHandler(content)
